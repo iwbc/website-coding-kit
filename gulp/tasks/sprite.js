acb53e8b69a586bb21aa87838b0195e08c3de044
Dai Iwabuchi <d.iwabuchi@vanq.in>
2016-09-23 22:46:25 +0900

    [improve] Sprite画像をディレクトリごとに作成できるように改修
    spritesディレクトリをassetsに移動、この中のディレクトリごとにSprite画像を作成できる
    また、ディレクトリ名末尾に-2xをつけることで、Retina用のSprite画像を作成できる
    全てのSpriteクラスを書き出すsass/modules/_sprite.scssを作成

--- gulp/tasks/sprite.js
+++ gulp/tasks/sprite.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const gulp   = require('gulp');
+const path   = require('path');
 const ms     = require('merge-stream');
-const png    = require('imagemin-pngquant');
 const config = require('../config.js');
 const $      = require('../load.js');
 
@@ -11,11 +11,33 @@ const $      = require('../load.js');
  */
 
 gulp.task('sprite', () => {
-	const data = gulp.src(config.path.sprite.src)
-		.pipe($.plumber({errorHandler: $.notify.onError('<%= error.message %>')}))
-		.pipe($.spritesmith(config.sprite));
-	const image = data.img.pipe(gulp.dest(config.path.sprite.dest.image));
-	const style = data.css.pipe(gulp.dest(config.path.sprite.dest.style));
 
-	return ms(image, style);
+	return gulp.src(config.path.sprite.src)
+		.pipe($.flatmap((stream, file) => {
+			if (!file.isDirectory()) { return stream; }
+
+			const dirname       = file.path.split(path.sep).pop();
+			const relative_path = path.relative(config.path.style.dest, config.path.image.dest);
+			const is_retina     = /-2x$/.test(dirname);
+			const options    = {
+				cssTemplate        : 'gulp/templates/spritesmith.handlebars',
+				cssSpritesheetName : dirname,
+				imgName            : `${dirname}.png`,
+				cssName            : `_${dirname}.scss`,
+				imgPath            : `${relative_path}/${dirname}.png`,
+				algorithm          : 'binary-tree',
+				padding            : 6,
+				cssOpts            : {
+					scale  : is_retina ? .5 : 1
+				}
+			};
+
+			const data = gulp.src(`${file.path}/*.png`)
+				.pipe($.plumber({errorHandler: $.notify.onError('<%= error.message %>')}))
+				.pipe($.spritesmith(options));
+			const image = data.img.pipe(gulp.dest(config.path.sprite.dest.image));
+			const style = data.css.pipe(gulp.dest(config.path.sprite.dest.style));
+
+			return ms(image, style);
+		}));
 });
