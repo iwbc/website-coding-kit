ba6e9e7c40d9f613d03d2848c304538b3469c20f
iwbc <1209760+iwbc@users.noreply.github.com>
2016-09-29 00:01:38 +0900

    [feature] easymockによるモックサーバ機能を追加
    特定のリクエストパスをモックサーバに転送し、mockディレクトリの対応するデータを返す
    ex)
    GET /api/user → mock/user_get.json
    POST /api/profile → mock/profile_post.json

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -2,6 +2,9 @@
 
 const gulp   = require('gulp');
 const extend = require('extend');
+const url    = require('url');
+const mock   = require('easymock').MockServer;
+const proxy  = require('proxy-middleware');
 const config = require('../config.js');
 const $      = require('../load.js');
 
@@ -10,10 +13,21 @@ const $      = require('../load.js');
  */
 
 gulp.task('server', () => {
-	let bs_options = extend({}, config.server.browsersync);
-	bs_options.server = bs_options.server || {
-		baseDir : config.dest
+	const proxy_pass    = url.parse(config.server.mock.proxy.pass);
+	let proxy_options   = proxy_pass;
+	proxy_options.route = config.server.mock.proxy.location;
+
+	let bs_options      = extend({}, config.server.browsersync);
+	bs_options.server   = bs_options.server || {
+		baseDir    : config.dest,
+		middleware : [proxy(proxy_options)]
 	}
 	if (bs_options.proxy) { delete bs_options.server }
+
+	new mock({
+		port : proxy_pass.port,
+		path : config.server.mock.path
+	}).start();
+
 	return $.browser.init(bs_options);
 });
