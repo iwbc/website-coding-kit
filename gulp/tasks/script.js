b79d3d3020f843a0fed9a5b81ec1e2f49d2b5663
iwbc <1209760+iwbc@users.noreply.github.com>
2016-10-03 03:14:28 +0900

    [update] jsディレクトリ直下のjsファイルごとにbundleできるように改修

--- gulp/tasks/script.js
+++ gulp/tasks/script.js
@@ -1,38 +1,54 @@
 'use strict';
 
-const gulp    = require('gulp');
-const path    = require('path');
-const rollup  = require('rollup').rollup;
-const resolve = require('rollup-plugin-node-resolve');
-const cjs     = require('rollup-plugin-commonjs');
-const buble   = require('rollup-plugin-buble');
-const uglify  = require('rollup-plugin-uglify');
-const config  = require('../config.js');
-const $       = require('../load.js');
+const gulp      = require('gulp');
+const path      = require('path');
+const through2  = require('through2');
+const rollup    = require('rollup').rollup;
+const resolve   = require('rollup-plugin-node-resolve');
+const cjs       = require('rollup-plugin-commonjs');
+const buble     = require('rollup-plugin-buble');
+const uglify    = require('rollup-plugin-uglify');
+const config    = require('../config.js');
+const $         = require('../load.js');
 
 /**
  * JSのモジュール依存解決、ES5へのトランスパイル
  */
 
-gulp.task('script', () => {
-	const module_name = path.basename(config.path.script.src, '.js');
-	let plugins = [
-		resolve(),
-		cjs(),
-		buble(config.script.buble)
-	];
-	if (config.build.js_minify) { plugins.push(uglify({ preserveComments : 'some' })); }
+gulp.task('script', ['script:__rollup'], () => {
+	$.browser.reload();
+});
 
-	return rollup({
-		entry   : config.path.script.src,
-		plugins : plugins
-	}).then((bundle) => {
-		$.browser.reload();
-		return bundle.write({
-			format     : 'iife',
-			sourceMap  : config.build.sourcemap,
-			dest       : `${config.path.script.dest}/${module_name}.js`,
-			moduleName : module_name
-		});
-	});
+gulp.task('script:__rollup', () => {
+	return gulp.src(config.path.script.src)
+		.pipe($.plumber({errorHandler: $.notify.onError('<%= error.message %>')}))
+		.pipe(through2.obj((file, encoding, callback) => {
+
+			if (file.isDirectory()) { callback(); }
+
+			const module_name = path.basename(file.path, '.js');
+			let plugins = [
+				resolve(),
+				cjs(),
+				buble(config.script.buble)
+			];
+			if (config.build.js_minify) { plugins.push(uglify({ preserveComments : 'some' })); }
+
+			rollup({
+				entry: file.path,
+				plugins: plugins
+			}).then((bundle) => {
+				bundle.write({
+					format     : 'iife',
+					sourceMap  : config.build.sourcemap,
+					dest       : `${config.path.script.dest}/${module_name}.js`,
+					moduleName : module_name
+				}).then(() => {
+					callback(null, file);
+				});
+			}).catch((err) => {
+				callback(err);
+			});
+
+		}));
 });
