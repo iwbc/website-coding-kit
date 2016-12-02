0b104a28d8e34f0cf098fd345296dabdff0aff7c
Dai Iwabuchi <d.iwabuchi@vanq.in>
2016-12-02 16:46:37 +0900

    [change] sass/cssの構成をFLOCSSベースの設計ガイドラインに沿うよう変更
    sassディレクトリ内の構成を変更
    sass function/mixinをリライト
    sass function/mixinのテストを作成（mocha）
    scssのコンパイル時にSassDocを出力するよう機能追加
    スプライト画像のサンプルデータ追加

--- gulp/tasks/style.js
+++ gulp/tasks/style.js
@@ -1,24 +1,39 @@
 'use strict';
 
-const gulp     = require('gulp');
-const config   = require('../config.js');
-const $        = require('../load.js');
+const gulp    = require('gulp');
+const extend  = require('extend');
+const ms      = require('merge-stream');
+const sassdoc = require('sassdoc');
+const config  = require('../config.js');
+const $       = require('../load.js');
 
 /**
- * SassのLint、コンパイル、minify化
+ * SassのLint、コンパイル、minify化、SassDocの生成
  */
 
 gulp.task('style', () => {
-	return gulp.src(config.path.style.src)
+
+	let sassdoc_options = extend({}, config.style.sassdoc);
+	sassdoc_options.dest = config.path.style.dest.sassdoc;
+
+	const css = gulp.src(config.path.style.src)
 		.pipe($.plumber({errorHandler: $.notify.onError('<%= error.message %>')}))
 		.pipe($.sassLint())
         .pipe($.sassLint.format())
         .pipe($.sassLint.failOnError())
 		.pipe($.if(config.build.sourcemap, $.sourcemaps.init()))
+		.pipe($.sassGlob())
 		.pipe($.sass(config.style.sass))
 		.pipe($.pleeease(config.style.pleeease))
 		.pipe($.if(config.build.css_minify, $.csso()))
 		.pipe($.if(config.build.sourcemap, $.sourcemaps.write()))
-		.pipe(gulp.dest(config.path.style.dest))
+		.pipe(gulp.dest(config.path.style.dest.css))
 		.pipe($.browser.stream());
+
+	const doc = gulp.src(config.path.style.src)
+		.pipe($.plumber({errorHandler: $.notify.onError('<%= error.message %>')}))
+		.pipe($.sassGlob())
+		.pipe(sassdoc(sassdoc_options));
+
+	return ms(css, doc);
 });
