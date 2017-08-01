ca234f74cf5b31d923517708dac24ba0a6cbd2a9
iwbc <1209760+iwbc@users.noreply.github.com>
2017-08-01 17:21:29 +0900

    browser-syncをSPAに対応

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -1,9 +1,10 @@
 'use strict';
 
-const gulp   = require('gulp');
-const extend = require('extend');
-const config = require('../config.js');
-const $      = require('../load.js');
+const gulp    = require('gulp');
+const extend  = require('extend');
+const history = require('connect-history-api-fallback');
+const config  = require('../config.js');
+const $       = require('../load.js');
 
 /**
  * サーバーの起動
@@ -11,17 +12,16 @@ const $      = require('../load.js');
 
 gulp.task('server', () => {
 
-  let bs_options = extend({}, config.server.browsersync);
+  let bs_options = extend(true, {
+    server: {
+      baseDir    : config.dest,
+      index      : 'index.html',
+      routes     : {},
+      middleware : [history()]
+    }
+  }, config.server.browsersync);
 
-  bs_options.server = bs_options.server || { baseDir : config.dest }
-
-  // serverが未定義の場合はdestとsassdocsディレクトリをサーブする
-  if (bs_options.server) {
-    bs_options.server.baseDir = config.dest;
-    bs_options.server.routes = {};
-    bs_options.server.routes[`/${config.path.style.dest.sassdoc}`] = config.path.style.dest.sassdoc;
-  }
-
+  bs_options.server.routes[`/${config.path.style.dest.sassdoc}`] = config.path.style.dest.sassdoc;
   if (bs_options.proxy) { delete bs_options.server }
   return $.browser.init(bs_options);
 
