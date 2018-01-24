221fc8cda4505d02ff66e74029718480057a7b57
iwbc <1209760+iwbc@users.noreply.github.com>
2018-01-24 18:51:22 +0900

    feat(gulp): EJSに環境変数(__path)を追加

--- gulp/tasks/ejs.js
+++ gulp/tasks/ejs.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path   = require('path');
 const gulp   = require('gulp');
 const config = require('../config.js');
 const $      = require('../load.js');
@@ -11,6 +12,23 @@ const $      = require('../load.js');
 gulp.task('ejs', () => {
   return gulp.src(config.path.ejs.src)
     .pipe($.plumber({errorHandler: $.notify.onError('<%= error.message %>')}))
+    .pipe($.data((file) => {
+      const src      = path.join(global.projectPath, config.src);
+      const filename = file.path.replace(src, '');
+      const dirname  = path.dirname(filename);
+      const basename = path.basename(filename);
+      const extname  = path.extname(filename);
+      const relative = path.relative(dirname, '/');
+      return {
+        __path : {
+          filename : filename.replace(/\\/g, '/'),
+          dirname  : dirname.replace(/\\/g, '/'),
+          basename : basename,
+          extname  : extname,
+          relative : relative ? relative.replace(/\\/g, '/') + '/' : ''
+        }
+      }
+    }))
     .pipe($.ejs({}, {}, { ext : '.html' })).on('error', function() { this.emit('end'); })
     .pipe(gulp.dest(config.path.ejs.dest))
     .pipe($.browser.stream());
