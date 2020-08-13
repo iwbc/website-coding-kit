1fa10254555e3d9fec9eb1a8d520cad02515856a
iwbc <1209760+iwbc@users.noreply.github.com>
2020-08-14 03:15:38 +0900

    style: lint

--- gulp/config.js
+++ gulp/config.js
@@ -1,19 +1,19 @@
-'use strict';
+'use strict'
 
-const minimist = require('minimist');
-let config     = require('../gulp.config.js');
+const minimist = require('minimist')
+let config = require('../gulp.config.js')
 
 /**
  * コマンドの引数から設定の書き換え
  */
 
 const argv = minimist(process.argv.slice(2), {
-  string  : ['env'],
-  default : {
-    env : 'development'
-  }
-});
+  string: ['env'],
+  default: {
+    env: 'development',
+  },
+})
 
-config.ENV     = argv.env;
-config.build   = config.build[config.ENV] || config.build.default;
-module.exports = config;
+config.ENV = argv.env
+config.build = config.build[config.ENV] || config.build.default
+module.exports = config
--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -1,27 +1,31 @@
-'use strict';
+'use strict'
 
-const gulp    = require('gulp');
-const extend  = require('extend');
-const config  = require('../config');
-const $       = require('../load');
+const gulp = require('gulp')
+const extend = require('extend')
+const config = require('../config')
+const $ = require('../load')
 
 /**
  * サーバーの起動
  */
 
 gulp.task('server', (callback) => {
+  let bs_options = extend(
+    true,
+    {
+      server: {
+        baseDir: config.dest,
+        index: 'index.html',
+        routes: {},
+      },
+    },
+    config.server.browsersync
+  )
 
-  let bs_options = extend(true, {
-    server: {
-      baseDir : config.dest,
-      index   : 'index.html',
-      routes  : {}
-    }
-  }, config.server.browsersync);
-
-  bs_options.server.routes[`/${config.path.style.dest.sassdoc}`] = config.path.style.dest.sassdoc;
-  if (bs_options.proxy) { delete bs_options.server }
-  $.browser.init(bs_options);
-  callback();
-
-});
+  bs_options.server.routes[`/${config.path.style.dest.sassdoc}`] = config.path.style.dest.sassdoc
+  if (bs_options.proxy) {
+    delete bs_options.server
+  }
+  $.browser.init(bs_options)
+  callback()
+})
