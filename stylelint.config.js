e790e3967581944b7a2229f52742d18e131cedef
iwbc <1209760+iwbc@users.noreply.github.com>
2022-01-27 00:11:25 +0900

    chore: scssのlintルール変更

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['stylelint-config-standard-scss', 'stylelint-prettier/recommended', 'stylelint-config-recess-order'],
   rules: {
     'selector-class-pattern': null,
+    'scss/dollar-variable-pattern': '^_?[-a-z0-9]+$',
   },
 }
